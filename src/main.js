e7bc28f394329d6e744abb85b147c4724ff995c1
Rostislav <125198729+Zelenro@users.noreply.github.com>
2026-05-14 10:20:37 +0300

    api-books

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { headerJS } from './js/header';
+import { booksApiService } from './js/api-books';
 
 document.addEventListener('DOMContentLoaded', () => {
   headerJS();
 });
+
+booksApiService.getCategori().then(resp => console.log(resp));
